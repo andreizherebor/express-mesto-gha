4fbde370864171026a1f7823f6cca3b9544aaf59
Andrey Zherebor <andreizherebor@yandex.ru>
2023-06-08 16:47:44 +0700

    ПР14 первая проверка

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const { createUser, login } = require('./controllers/users');
+const {
+  validationCreateUser,
+  validationLogin,
+} = require('./middlewares/validations');
+const auth = require('./middlewares/auth');
+const handelError = require('./middlewares/handelError');
 const routes = require('./routes');
-
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  useNewUrlParser: true,
-});
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  req.user = {
-    _id: '64761ad4448ca682a7383365',
-  };
 
-  next();
-});
+app.post('/signin', validationLogin, login);
+app.post('/signup', validationCreateUser, createUser);
 
+app.use(auth);
 app.use(routes);
+app.use(errors());
+app.use(handelError);
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
-const { NOT_FOUND } = require('../utils/error');
+const NotFound = require('../errors/NotFoundError');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 
-router.use((req, res) => {
-  res.status(NOT_FOUND).send({ message: 'Сервер не найден' });
+router.use((req, res, next) => {
+  next(new NotFound('Запрашиваемая страница не существует'));
 });
 
 module.exports = router;
