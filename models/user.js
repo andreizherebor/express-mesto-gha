25787f5d5ad82d8448349d5102504fed373ad3b6
Andrey Zherebor <andreizherebor@yandex.ru>
2023-06-09 12:21:35 +0700

    исправления автотестов

--- models/user.js
+++ models/user.js
@@ -1,8 +1,9 @@
+/* eslint-disable func-names */
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const isUrl = require('validator/lib/isURL');
 const isEmail = require('validator/lib/isEmail');
 const AuthError = require('../errors/AuthError');
-const { validateUrl } = require('../utils/validateUrl');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -20,7 +21,10 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-    validate: { validator: validateUrl, message: 'Введите валидную ссылку' },
+    validate: {
+      validator: (url) => isUrl(url),
+      message: 'Некорректный адрес URL',
+    },
   },
   email: {
     type: String,
@@ -39,17 +43,16 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// eslint-disable-next-line func-names
 userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new AuthError('Неправильная почта или пароль'));
+        return Promise.reject(new AuthError('Неправильные почта или пароль'));
       }
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new AuthError('Неправильная почта или пароль'));
+            return Promise.reject(new AuthError('Неправильные почта или пароль'));
           }
           return user;
         });
--- utils/validateUrl.js
+++
@@ -1,9 +0,0 @@
-function validateUrl(url) {
-  const regex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
-  if (regex.test(url)) {
-    return url;
-  }
-  throw new Error('Невалидная ссылка');
-}
-
-module.exports = { validateUrl };
